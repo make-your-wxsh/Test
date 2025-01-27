d96749903ac5c048b7f987aab0a673daddbdba80
Mori <dayspring0607@gmail.com>
2025-01-27 23:29:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// scripts/script.js
 
-// 모든 네비게이션 링크 선택
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault(); // 기본 링크 동작 방지
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth' // 부드러운 스크롤링
-        });
-    });
-});
